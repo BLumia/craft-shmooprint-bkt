cafc7d840df1d3045a7e1b9ec5edd22d62b53ff6
Gary Wang <wzc782970009@gmail.com>
2020-08-01 13:56:18 +0800

    test building qmidiplayer

---
+++ media-libs/rtmidi/rtmidi.py
@@ -0,0 +1,27 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        for ver in ['4.0.0']:
+            self.targets[ver] = f"https://github.com/thestk/rtmidi/archive/{ver}.tar.gz"
+            self.archiveNames[ver] = f"rtmidi-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"rtmidi-{ver}"
+
+        self.description = "A set of C++ classes that provide a common API for realtime MIDI input/output across Linux (ALSA & JACK), Macintosh OS X (CoreMIDI) and Windows (Multimedia)"
+        self.webpage = "http://www.music.mcgill.ca/~gary/rtmidi/"
+        self.defaultTarget = '4.0.0'
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["dev-utils/pkg-config"] = None
+        self.runtimeDependencies["libs/glib"] = None
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **args):
+        CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.args = "-D__WINDOWS_MM__='1'"
---
+++ qt-apps/qmidiplayer/qmidiplayer.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+
+    def setTargets(self):
+        self.svnTargets['master'] = 'https://github.com/chirs241097/QMidiPlayer.git'
+        for ver in ["0.8.7-1"]:
+            self.targets[ver] = f"https://github.com/chirs241097/QMidiPlayer/archive/{ver}.tar.gz"
+            self.archiveNames[ver] = f"qmidiplayer-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"QMidiPlayer-{ver}"
+        self.defaultTarget = "0.8.7-1"
+
+        self.description = "A free cross-platform midi file player based on libfluidsynth and Qt."
+        self.displayName = "QMidiPlayer"
+
+    def setDependencies(self):
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/qt5/qtsvg"] = None
+        self.runtimeDependencies["libs/fluidsynth"] = None
+        self.runtimeDependencies["media-libs/rtmidi"] = None
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **args):
+        CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.args = "-DBUILD_VISUALIZATION=OFF"
+
+    def createPackage(self):
+        # self.blacklist_file.append(os.path.join(self.packageDir(), 'blacklist.txt'))
+        self.defines["company"] = "Bear Kids Team"
+        self.defines["executable"] = "qmidiplayer.exe"
+        self.defines["license"] = os.path.join(self.sourceDir(), "COPYING")
+        self.defines["icon"] = os.path.join(self.sourceDir(), "img", "qmidiplayer.ico")
+        self.ignoredPackages.append("binary/mysql")
+        self.ignoredPackages.append("libs/dbus")
+        return TypePackager.createPackage(self)
